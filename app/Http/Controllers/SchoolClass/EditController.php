acbb35005de27dd4e959868663f50dbb633d75a1
DanielPNK <danielpnkkdw@gmail.com>
2026-08-11 08:38:41 +0700

    feat: implement views for schoolclass

--- app/Http/Controllers/SchoolClass/EditController.php
+++ app/Http/Controllers/SchoolClass/EditController.php
@@ -12,6 +12,9 @@ class EditController extends Controller
      */
     public function __invoke(Request $request, string $id)
     {
-        return "Ini adalah halaman edit kelas dengan ID: {$id}";
+        $title = "Sistem Sekolah - Edit Kelas";
+        return view('classes.edit', [
+            'title' => $title
+        ]);
     }
 }
--- app/Http/Controllers/SchoolClass/ShowController.php
+++ app/Http/Controllers/SchoolClass/ShowController.php
@@ -12,6 +12,9 @@ class ShowController extends Controller
      */
     public function __invoke(Request $request, string $id)
     {
-        return "Menampilkan detail kelas dengan ID: {$id}";
+        $title = "Sistem Sekolah - Detail Kelas";
+        return view('classes.show', [
+            'title' => $title
+        ]);
     }
 }
